5c54b46baada47b11ec4d2b662203c760eaf9079
John Marvin NDEKEBITIK HELIANG <92511830+JohnMarvin237@users.noreply.github.com>
2024-04-01 12:00:02 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,10 @@
+﻿namespace hello_github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub! Je m'appelle NDEKEBITIK HELIANG John Marvin");
+        }
+    }
+}
